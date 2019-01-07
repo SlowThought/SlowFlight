bc58e73303bfc16fb497c3f41243026bb3497afb
Patrick King <slowthought@gmail.com>
2019-01-07 15:32:48 -0500

    potential.rkt fixed
    Turns out a and |a| are the same thing. Replaced |a| with na ("norm of a")

--- Private/Inviscid/potential.rkt
+++ Private/Inviscid/potential.rkt
@@ -4,31 +4,56 @@
    Functions for describing the inviscid flow around a circle are developed using complex potentials.
 |#
 (require racket/math) ; for complex functions
+(provide make-circle
+         cylinder-parameters
+         Φ_cylinder
+         dΦ/dz_cylinder)
 (module+ test
   (printf "potential.rkt tests running.~n")
   (require plot
-           "../Utility/plots.rkt"))
+           "../Utility/plots.rkt")
+  ; For sake of clarity in some plots, and irrelevance to the eventual application,
+  ; we suppress the display of velocity inside certain regions.
+  (define (suppress-f-within-r-of-c f r c) ; f complex->complex, r & c complex, returned function vector->vector
+    (λ(x y)
+      (let [(z (make-rectangular x y))]
+        (if (< (magnitude (- z c)) r) #(0 0)
+             (complex->vector (f z)))))))
+
+;; First, we define the cylinder about which we wish to determine the flow
+(define (make-circle z0 r0)
+         (λ (θ)(+ (* r0 (exp (* 0+i θ))) z0)))
+
+(module+ test
+  (define c0 1+i)
+  (define a 1-i)
+  (define c (make-circle c0 a))
+  (plot (parametric (λ (t) (complex->vector (c t))) 0 6.29))
+  (printf "~nYou should see a circle above.~n"))
 
 ;; Velocity in the far field is just uniform flow of magnitude 1
 (define (V∞ α) (exp (* 0+i α))) 
 
 ;; The potential function and its derivative use the conjugate of the velocity (C-R vs continuity)
-(define (Φ_Uniform_Flow Vinf c0)(λ(z)(* (conjugate Vinf) (- z c0)))) ; the somewhat arbitrary subtraction of c0 is convenient later
+;; This is most obvious in uniform flow.
+
+(define (Φ_Uniform_Flow Vinf c0)(λ(z)(* (conjugate Vinf) (- z c0))))
 (define (dΦ/dz_UF Vinf)(λ(z)(conjugate Vinf)))
 
 (module+ test
   (define alpha 0.3)
   (define Vinf (V∞ alpha))
-  (define c0 1+1i)
   (define UF (Φ_Uniform_Flow Vinf c0))
   (define dUF (dΦ/dz_UF Vinf))
   (display
-   (plot (list (contours (λ(x y)(imag-part (UF (make-rectangular x y)))) #:levels 5)
+   (plot (list (parametric (λ (t) (complex->vector (c t))) 0 6.29)
+               (contours (λ(x y)(let[(z (make-rectangular x y))](imag-part (UF (make-rectangular x y)))))
+                         #:levels 5)
                (vector-field (λ(x y)(complex->vector (conjugate (dUF (make-rectangular x y)))))))
-         #:x-min 0
-         #:x-max 2
-         #:y-min 0
-         #:y-max 2))
+         #:x-min -1
+         #:x-max 3
+         #:y-min -1
+         #:y-max 3))
   (printf "~nUniform flow, shown two ways.~n"))
 
 
@@ -39,38 +64,35 @@
 (module+ test
   (define Dub (Φ_Doublet alpha 1 c0))
   (define dDub(dΦ/dz_Dub alpha 1 c0))
-  (display
-   (plot (list (contours (λ(x y)(imag-part (Dub (make-rectangular x y)))) #:levels 15)
-               (vector-field (λ(x y)(complex->vector (conjugate (dDub (make-rectangular x y)))))
-                             #:scale 0.01))
-         #:x-min 0
-         #:x-max 2
-         #:y-min 0
-         #:y-max 2))
+  (display (plot (list (parametric (λ (t) (complex->vector (c t))) 0 6.29)
+                       (contours (λ (x y) (imag-part (Dub (make-rectangular x y))))
+                                 #:levels 11)
+                       (vector-field (suppress-f-within-r-of-c (λ(z)(conjugate (dDub z))) 0.5 c0)
+                                     #:scale 0.1))
+                 #:x-min -1
+                 #:x-max 3
+                 #:y-min -1
+                 #:y-max 3))
   (printf "~nDoublet flow, shown two ways.~n")
   #| A uniform flow and a doublet combined should yield the flow field about a cylinder. Say that cylinder
-     is of radius |a|, centered on c0, where a and c0 are complex number (making a complex will prove convenient later).
+     is of radius |a|, centered on c0, where a and c0 are complex numbers (making a complex will prove convenient later).
      γ should be |a|^2.
   |#
-  (define a 0.5)
-  (set! Dub (Φ_Doublet alpha (* a a) c0))
-  (set! dDub (dΦ/dz_Dub alpha (* a a) c0))
-  (display
-   (let [(f (λ(z)(+ (UF z)(Dub z))))
-         (df (λ(z)(+ (dUF z)(dDub z))))]
-     (plot (list (contours (λ(x y)(imag-part (f (make-rectangular x y)))) #:levels 15)
-                 (vector-field (λ(x y)
-                                 ; For sake of clarity in the plot, and irrelevance to the eventual application,
-                                 ; we suppress the display of velocity inside the cylinder
-                                 (if (< (+ (* (sub1 x)(sub1 x))(* (sub1 y)(sub1 y)))(* a a))
-                                     #(0 0)
-                                     (complex->vector (conjugate (df (make-rectangular x y))))))
-                               #:scale .1))
-           #:x-min 0
-           #:x-max 2
-           #:y-min 0
-           #:y-max 2)))
-  (printf "~nFlow about a cylinder, no circulation.~n"))
+  (let [(na^2 (*(magnitude a)(magnitude a)))]
+    (set! Dub (Φ_Doublet alpha na^2 c0))
+    (set! dDub (dΦ/dz_Dub alpha na^2 c0))
+    (display
+     (let [(f (λ(z)(+ (UF z)(Dub z))))
+           (df (λ(z)(+ (dUF z)(dDub z))))]
+       (plot (list (contours (λ(x y)(imag-part (f (make-rectangular x y)))) #:levels 15)
+                   (vector-field (suppress-f-within-r-of-c (λ (z)(conjugate (df z))) (magnitude a) c0)
+                                 #:scale .2)
+                   (parametric (λ (t) (complex->vector (c t))) 0 6.29))
+           #:x-min -2
+           #:x-max 4
+           #:y-min -2
+           #:y-max 4)))
+  (printf "~nFlow about a cylinder, no circulation.~n")))
 
 ;; Vortex centered on c0
 (define (Φ_Vortex Γ c0) (λ(z) (* 0+i Γ (log (- z c0)))))
@@ -93,35 +115,32 @@
    are already available. |#
 
 (define (cylinder-parameters a)
-  (let*[(|a| (magnitude a))
-        {|a|^2 (* a a)}
+  (let*[(na (magnitude a))
+        {na^2 (* na na)}
         (α0L (angle a))]
-    (values |a| |a|^2 α0L)))
+    (values na na^2 α0L)))
 
-(define (Φ_cylinder c0 |a| |a|^2 α α0L)
+(define (Φ_cylinder c0 na na^2 α α0L)
   (λ(z)(+ ((Φ_Uniform_Flow (V∞ α) c0) z)
-          ((Φ_Doublet α |a|^2 c0) z)
-          ((Φ_Vortex (* 2 |a| (sin (- α α0L))) c0) z))))
+          ((Φ_Doublet α na^2 c0) z)
+          ((Φ_Vortex (* 2 na (sin (- α α0L))) c0) z))))
 
 
-(define (dΦ/dz_cylinder c0 |a| |a|^2 α α0L)
+(define (dΦ/dz_cylinder c0 na na^2 α α0L)
   (λ(z)(+ ((dΦ/dz_UF (V∞ α)) z)
-          ((dΦ/dz_Dub α |a|^2 c0) z)
-          ((dΦ/dz_Vortex (* 2 |a| (sin (- α α0L))) c0) z))))
+          ((dΦ/dz_Dub α na^2 c0) z)
+          ((dΦ/dz_Vortex (* 2 na (sin (- α α0L))) c0) z))))
 
 (module+ test
-  (let-values ([(|a| |a|^2 α0L)(cylinder-parameters a)])
-    (display (plot (list (contours (λ(x y)(imag-part ((Φ_cylinder c0 |a| |a|^2 alpha α0L)(make-rectangular x y))))
-                                   #:levels 20)
-                         (vector-field (λ(x y) ; For sake of clarity in the plot, and irrelevance to the eventual application,
-                                               ; we suppress the display of velocity inside the cylinder
-                                               (if (< (+ (* (sub1 x)(sub1 x))(* (sub1 y)(sub1 y)))(* a a))
-                                                   #(0 0)
-                                                   (complex->vector
-                                                    (conjugate
-                                                     ((dΦ/dz_cylinder c0 |a| |a|^2 alpha α0L)(make-rectangular x y))))))))
-                 #:x-min 0
-                 #:x-max 2
-                 #:y-min 0
-                 #:y-max 2)))
+  (define-values (na na^2 α0L)(cylinder-parameters a))
+  (printf "a is ~s~n|a| is ~s~n|a|^2 is ~s~nα0L is ~s~n" a na na^2 α0L)
+  (display (plot (list (parametric (λ (t) (complex->vector (c t))) 0 6.29)
+                       (contours (λ(x y)(imag-part ((Φ_cylinder c0 na na^2 alpha α0L)(make-rectangular x y))))
+                                 #:levels 20)
+                       (vector-field (suppress-f-within-r-of-c
+                                      (λ(z)(conjugate((dΦ/dz_cylinder c0 na na^2 alpha α0L)z))) na c0)))
+                 #:x-min -2
+                 #:x-max 4
+                 #:y-min -2
+                 #:y-max 4))
   (printf "~nFlow about a cylinder with circulation.~n"))
